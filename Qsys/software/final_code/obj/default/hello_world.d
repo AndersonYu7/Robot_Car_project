1de893b76849cb75542337e62570c7dc1b445e1a
Andersonyu7 <andersonyu614@gmail.com>
2023-05-18 11:37:33 +0800

    upload final_code

---
+++ Qsys/software/final_code/obj/default/hello_world.d
@@ -0,0 +1,11 @@
+obj/default/hello_world.o: hello_world.c ../final_code_bsp//HAL/inc/io.h \
+  ../final_code_bsp//HAL/inc/alt_types.h ../final_code_bsp/system.h \
+  ../final_code_bsp/linker.h
+
+../final_code_bsp//HAL/inc/io.h:
+
+../final_code_bsp//HAL/inc/alt_types.h:
+
+../final_code_bsp/system.h:
+
+../final_code_bsp/linker.h:
